ff943010a466261ca5a23c1b1866944050712adb
Charlie Harding <charlie_harding@icloud.com>
2022-05-09 19:35:30 +0200

    Fix sitemap not existing

--- build/write_if_changed.rb
+++ build/write_if_changed.rb
@@ -1,10 +1,17 @@
 require 'digest'
 
 def write_if_changed(file, output, log: false)
-  previous_digest = Digest::SHA256.file(file).digest
-  new_digest = Digest::SHA256.digest(output)
+  begin
+    previous_digest = Digest::SHA256.file(file).digest
+  rescue Errno::ENOENT # File does not exist yet
+    file_changed = true
+  else
+    new_digest = Digest::SHA256.digest(output)
+    file_changed = previous_digest == new_digest
+  end
+
   File.write(file, output)
-  if previous_digest == new_digest
+  if file_changed
     puts "Unchanged:     #{file}" if log
     return false
   else
